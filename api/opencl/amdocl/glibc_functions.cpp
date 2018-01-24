437b9117b4c23d24fcae628be72e45f16bfba2af
foreman <dl.constructicon@amd.com>
2018-01-23 19:42:53 -0500

    P4 to Git Change 1506952 by lmoriche@lmoriche_opencl_dev2 on 2018/01/23 19:37:39
    	SWDEV-1 - Cleanups required to build with gcc-5.4
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/api/opencl/amdocl/build/Makefile.api#172 edit
    ... //depot/stg/opencl/drivers/opencl/api/opencl/amdocl/glibc_functions.cpp#2 edit

--- api/opencl/amdocl/glibc_functions.cpp
+++ api/opencl/amdocl/glibc_functions.cpp
@@ -6,11 +6,13 @@
 extern "C" {
 #endif // __cplusplus
 
+#if defined(_LP64)
 asm (".symver memcpy, memcpy@GLIBC_2.2.5");
 void *__wrap_memcpy(void *dest, const void *src, size_t n)
 {
     return memcpy(dest, src, n);
 }
+#endif // _LP64
 
 #if defined(__cplusplus)
 }
