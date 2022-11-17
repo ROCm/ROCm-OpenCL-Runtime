58ba4a2bee5244bb7abaa9595e1db231604a8630
Ranjith Ramakrishnan <Ranjith.Ramakrishnan@amd.com>
2022-11-16 17:17:54 -0800

    SWDEV-366823 - Change pragma message to warning
    File reorganization feature was implemented with backward compatibility
    The backward compatibility support will be deprecated in future release.
    Changed the #pragma message to #warning for a smooth transition
    
    Change-Id: If917a0bef9bbc347f08ca02435d70d517f175afc

--- opencl-backward-compat.cmake
+++ opencl-backward-compat.cmake
@@ -48,7 +48,20 @@ function(create_header_template)
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
    THE SOFTWARE.
-   */\n\n#ifndef @include_guard@\n#define @include_guard@ \n\n#pragma message(\"This file is deprecated. Use file from include path /opt/rocm-ver/include/ and prefix with CL\")\n@include_statements@ \n\n#endif")
+   */
+
+#ifndef @include_guard@
+#define @include_guard@
+
+#if defined(_MSC_VER)
+#pragma message(\"This file is deprecated. Use file from include path /opt/rocm-ver/include/ and prefix with CL\")
+#elif defined(__GNUC__)
+#warning \"This file is deprecated. Use file from include path /opt/rocm-ver/include/ and prefix with CL\"
+#endif
+
+@include_statements@
+
+#endif")
 endfunction()
 
 
